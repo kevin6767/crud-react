ede9aa5d3d66f928832d1dfac3e4219ff88934ee
Kevin Eslick <eslick.kevin@yahoo.com>
2020-09-21 18:53:17 -0500

    making clean build for gh pages

---
+++ docs/build/precache-manifest.383fc9ad5b5b149c61372ec1405ce736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74984f96e09b85af050e81d955c7aa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eed5e26131f9fba5384",
+    "url": "/static/css/main.7461e616.chunk.css"
+  },
+  {
+    "revision": "7979273105e9e95ae7d7",
+    "url": "/static/js/2.4d4164ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d4164ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eed5e26131f9fba5384",
+    "url": "/static/js/main.84494e67.chunk.js"
+  },
+  {
+    "revision": "3de00e9a73f525ee4a46",
+    "url": "/static/js/runtime-main.39c4887f.js"
+  }
+]);
